a2050fb5353adba4be32d8542b236c453d5403ad
clairelinjen <61124312+clairelinjen@users.noreply.github.com>
2021-03-30 13:54:42 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,158 @@
+var livecode;       // textarea
+var playbutton;
+var stopbutton;
+var errorbox;
+var score = [];     // arr of ReadLine objects
+var loops = {};
+var activeKeys = [];
+var beat = 1.5;
+var on = false;
+
+function midiToFreq(m) {
+    return Math.pow(2, (m - 69) / 12) * 440;
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    livecode = document.getElementById("livecode");
+    playbutton = document.getElementById("play");
+    playbutton.addEventListener("click", change);
+    stopbutton = document.getElementById("stop");
+    stopbutton.addEventListener("click", stopPlaying);
+    errorbox = document.getElementById("er");
+
+    var comp = audioCtx.createDynamicsCompressor();
+    comp.threshold.setValueAtTime(-50, audioCtx.currentTime);
+    comp.connect(audioCtx.destination);
+
+    class Loop{
+        constructor(readline){
+            this.notes = readline.notes;
+            this.osc = audioCtx.createOscillator();
+            this.gain = audioCtx.createGain();
+            this.nextNote = 0;
+            this.lastNoteEnd = 0;
+            this.setUp();
+        }
+
+        setUp(){
+            this.osc.connect(this.gain).connect(comp);
+            this.gain.gain.setValueAtTime(0, audioCtx.currentTime);
+            this.osc.start();
+        }
+    }
+
+    function updateScore(){
+        score = [];
+        errorbox.innerHTML = "";
+        var lines = livecode.value.split("\n");
+        var keys = {};
+        for(let i = 0; i < lines.length; i++){
+            if (lines[i].replace(/\s/g, '').length) {
+                var l = new ReadLine(lines[i]); // in readline.js
+                if (l.errors.length>0){
+                    error(l.errors, i+1);
+                }
+                else if (l.key in keys){
+                    error(["Duplicate key \'"+l.key+"\'",], i+1);
+                }
+                else{
+                    score.push(l);
+                    keys[l.key] = true;
+                }
+            }
+        }
+        console.log(score);
+    }
+
+    function error(arr, l){
+        for (let i=0; i < arr.length; i++){
+            var error = "ERROR: Line "+l+" - "+arr[i]+"<br>";
+            errorbox.innerHTML += error;
+        }
+    }
+
+    function change(){
+        updateScore();
+        var newKeys = [];
+        for (let i = 0; i < score.length; i++){
+            var line = score[i];
+            if (activeKeys.includes(line.key)){
+                loops[line.key].notes = line.notes;
+                loops[line.key].newBeats = line.beats;
+            }
+            else{
+                loops[line.key] = new Loop(line);
+            }
+            newKeys.push(line.key);
+        }
+        for (let i=0; i < activeKeys.length; i++){
+            if (!newKeys.includes(activeKeys[i])){
+                loops[activeKeys[i]].notes = [];
+            }
+        }
+        activeKeys = newKeys;
+    }
+
+    function stopPlaying(){
+        for (let i=0; i < activeKeys.length; i++){
+            loops[activeKeys[i]].del = true;
+        }
+        activeKeys = [];
+        score = [];
+    }
+
+    function scheduleNote(key){
+        var loop = loops[key];
+        var start = loop.lastNoteEnd;
+        if (loop.nextNote >= loop.notes.length - 1){
+            loop.nextNote = 0;
+        }
+        var index = loop.nextNote;
+        var note = loop.notes[index][0];
+        var dur = beat / loop.notes[index][1];
+        var g = loop.gain;
+        if (note != null && note !== "x"){
+            g.gain.setValueAtTime(0,start);
+            loop.osc.frequency.setValueAtTime(midiToFreq(note), start);
+            g.gain.linearRampToValueAtTime(.8, start + dur/9);
+            g.gain.setValueAtTime(.8, start + 8*dur/9);
+            g.gain.linearRampToValueAtTime(0, start + dur);
+        }
+        else{
+            g.gain.setValueAtTime(0,start + dur);
+        }
+        loop.nextNote += 1;
+        loop.lastNoteEnd += dur;
+    }
+
+    var nextBeat = audioCtx.currentTime;
+    console.log("bext beat "+nextBeat);
+
+    function scheduler() {
+        console.log("time "+audioCtx.currentTime);
+        while (nextBeat < audioCtx.currentTime + 0.1) {
+            nextBeat += beat;
+            console.log("next beat "+nextBeat);
+            for (var key in loops){
+                if (loops[key].notes === null){
+                    loops.delete(key);
+                    continue;
+                }
+                if (loops[key].lastNoteEnd === 0){
+                    loops[key].lastNoteEnd = nextBeat -= beat;
+                }
+                while (loops[key].lastNoteEnd < nextBeat) {
+                    scheduleNote(key);
+                }
+            }
+        }
+
+        timerID = window.setTimeout(scheduler, 30.0);
+    };
+
+    scheduler();
+
+});
+
---
+++ readline.js
@@ -0,0 +1,270 @@
+music_rnn = new mm.MusicRNN("https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/melody_rnn");
+music_rnn.initialize();
+
+
+
+class ReadLine {
+    constructor(line){
+        this.key = "";
+        this.errors = [];
+        this.arr = this.scan(line.split(""));
+        if (this.errors.length === 0){
+            this.setValues();
+        }
+    }
+
+    scan(l){
+        var temp = [];
+        var arr = [];
+        var symbolcount = 0;
+        var notecount = 0;
+
+        while (l.length > 0){
+            var c = l.shift();
+            if (c === "="){
+                if (temp.length > 0){
+                    this.key = temp.join("");
+                    temp = []
+                }
+                else{
+                    this.key = arr.pop();
+                }
+                if (arr.length > 0){
+                    this.errors.push("Key cannot contain spaces");
+                    break;
+                }
+            }
+            else if (c === "*"){
+                if (temp.length > 0){
+                    temp.push(c);
+                    arr.push(temp.join(""));
+                    temp = [];
+                }
+                else{
+                    arr[arr.length -1] = arr[arr.length -1].concat(c);
+                }
+                symbolcount += 1;
+            }
+            else if (c === " "){
+                if (temp.length > 0){
+                    arr.push(temp.join(""));
+                    temp = [];
+                }
+            }
+            else if (c === "(" || c === ")"){
+                if (temp.length > 0){
+                    arr.push(temp.join(""));
+                    temp = [];
+                }
+                arr.push(c);
+                symbolcount += 1;
+            }
+            else{
+                temp.push(c);
+                notecount += 1;
+                if (c === '-'){
+                    notecount -= 1;
+                }
+                if(c === 'x'){
+                    if (notecount <= 1){
+                        this.errors.push("Number of notes before x must be greater than 1");
+                        break;
+                    }
+                }
+            }
+        }
+        arr.push(temp.join(""));
+        if (this.key === ""){
+            this.errors.push("Missing key")
+        }
+        return arr;
+    }
+
+    setValues(){
+        var result = this.noteSchedule(this.arr);
+        if (this.errors.length > 0){
+            this.key = "";
+        }
+        else{
+            this.notes = result[0];
+            this.generate(0);
+            // a 'note' is a length 2 array, note[0] is freq, note[1] is denominator of the fraction of the beat
+            // [440, 2] means the note is 440 hz and lasts half a beat
+            this.beats = result[1];
+        }
+    }
+
+    noteSchedule(arr){
+        var schedule = [];
+        var beats = 0;
+
+        while (arr.length > 0){
+            var curr = arr.shift();
+            if (curr === ""){
+                //continue
+            }
+            else {
+                var c = this.specialChar(curr, arr);
+                if (c != null) {
+                    schedule = schedule.concat(c[0]);
+                    beats += c[1];
+                    arr = c[2];
+                } else {
+                    var n = parseInt(curr);
+                    if (isNaN(n) || n < 0 || n > 127) {
+                        if (curr === "x"){
+                            schedule.push(["x", 1]);
+                        }
+                        else{
+                            if (curr === "-"){
+                                schedule.push([null, 1]);
+                            }
+                            else{
+                                this.errors.push("\'"+curr+"\' is undefined")
+                                break;
+                            }
+                        }
+                    } else {
+                        schedule.push([n, 1]);
+                    }
+                    beats += 1;
+                }
+            }
+        }
+        return [schedule, beats, arr]
+    }
+
+    specialChar(curr, arr){
+        if (curr === "("){
+            return this.group(arr);
+        }
+        else if (curr.slice(curr.length-1) === '*'){
+            return this.sequence(curr, arr);
+        }
+        else{
+            return null;
+        }
+    }
+
+    sequence(curr, arr){
+        var fit = false;
+        var mult = 1;
+        var beats = 1;
+        if (curr.slice(curr.length-2) === '**'){
+            fit = true;
+            mult = parseInt(curr.slice(0,curr.length-2));
+        }
+        else{
+            mult = parseInt(curr.slice(0,curr.length-1));
+            beats = mult;
+        }
+
+        var notes = [];
+        curr = arr.shift();
+        if (curr === "(") {
+            var g = this.group(arr);
+            for (let i = 0; i < mult; i++) {
+                notes = notes.concat(g[0]);
+            }
+            arr = g[2];
+        }
+        else {
+            var a = [];
+            for (let i = 0; i < mult; i++) {
+                a.push(curr);
+            }
+            var sch = this.noteSchedule(a);
+            notes = sch[0];
+        }
+
+        if (fit){
+            for (let i = 0; i < notes.length; i++){
+                notes[i][1] *= mult;
+            }
+        }
+        return [notes, beats, arr]
+    }
+
+    group(arr){
+        var gr = [];
+        var curr = arr.shift();
+        var ins = [];
+        var index = -1;
+        var addBeats = 0;
+        while (arr.length > 0 && curr !== ")"){
+            gr.push(curr);
+            curr = arr.shift();
+            var c = this.specialChar(curr,arr);
+            if (c != null){
+                ins = c[0];
+                addBeats = c[1];
+                arr = c[2];
+                curr = arr.shift();
+                index = gr.length;
+            }
+        }
+
+        var notes = [];
+        var beats;
+        if (arr.length === 0 && curr !== ")"){
+                // should throw an error
+            beats = 0;
+        }
+        else{
+            var sch = this.noteSchedule(gr);
+            for (let i = 0; i < sch[0].length; i++){
+                if (i === index){
+                    notes = notes.concat(ins);
+                }
+                notes.push(sch[0][i])
+            }
+            if (index === notes.length){
+                notes = notes.concat(ins);
+            }
+            beats = 1;
+            var denom = sch[1] + addBeats;
+
+            for (let i = 0; i < notes.length; i++){
+                notes[i][1] *= denom;
+            }
+        }
+        return [notes, beats, arr];
+    }
+
+    generate(start){
+        for (let i=start; i < this.notes.length; i++){
+            if (this.notes[i][0]==="x"){
+                this.genNotes(this.notes.slice(0,i));
+                break;
+            }
+        }
+    }
+
+    genNotes(notes) {
+
+        var fnotes = format(notes);
+        const qns = mm.sequences.quantizeNoteSequence(fnotes, 1);
+        var rnn_steps = notes.length + 1;
+        var rnn_temperature = 1.1;
+
+        music_rnn
+            .continueSequence(qns, rnn_steps, rnn_temperature)
+            .then((sample) => {
+                var result = mm.sequences.unquantizeSequence(sample);
+                this.notes[notes.length][0] = result.notes[0].pitch;
+                this.generate(notes.length+1)
+            })
+    }
+}
+
+function format(n) {
+    var steps = [];
+    var st = 0.0;
+    var end = 0.0;
+    for (let i = 0; i < n.length; i++){
+        end = st + 1/n[i][1];
+        steps.push({pitch: n[i][0], startTime: st, endTime: end});
+        st = end;
+    }
+    return {notes: steps, totalTime: end};
+}
